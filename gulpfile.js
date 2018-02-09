21cbd40e39eaece890fdcb4ed644fd4c7b5cc2c8
cc <un_chengcheng@outlook.com>
2018-02-09 09:10:28 +0800

    [normal] 添加服务监听文件改动

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require('gulp-cssmin');
+var connect = require('gulp-connect');
 
 gulp.task('compile', function() {
+  console.log('compile css')
   return gulp.src('./src/zhanwang/*.scss')
     .pipe(sass.sync())
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
     }))
-    .pipe(cssmin())
-    .pipe(gulp.dest('./lib/zhanwang'));
+    // .pipe(cssmin())
+    .pipe(gulp.dest('../../product/doctorwx/src/page/_doctor/'))
+    // .pipe(gulp.dest('./lib/zhanwang'))
+    .pipe(connect.reload())
 });
 
 gulp.task('copyfont', function() {
@@ -22,4 +26,14 @@ gulp.task('copyfont', function() {
     .pipe(gulp.dest('./lib/fonts'));
 });
 
-gulp.task('build', ['compile', 'copyfont']);
+gulp.task('auto', function() {
+  gulp.watch('./src/*/*.scss',['compile'])
+})
+
+gulp.task('server', function() {
+  connect.server({
+    root:'test',
+    livereload: true
+  })
+})
+gulp.task('build', ['server','auto']);
